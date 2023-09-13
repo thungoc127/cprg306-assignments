bad5151b9b2231a41df8f78145e00a0b8f1c383d
thungoc127 <thungoc127@gmail.com>
2023-09-13 13:25:55 -0600

    Assignment 2 Init
    Assignment 2 Init

---
+++ app/week2/studentInfo.js
@@ -0,0 +1,9 @@
+export default function EventList() {
+    return (
+    <div>
+        <h1>Name: Thu Ngoc Nguyen</h1>
+        <h1>Section: CPRG 306 E</h1>
+        <h1>GitHub: https://github.com/thungoc127</h1>
+    </div>
+    );
+  }
